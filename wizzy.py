b94528f7d4c62fbac1254cc1787a3a9567a1a73b
mujtaba-ali <im.muj@icloud.com>
2025-05-30 12:41:17 +0530

    create hero class

---
+++ wizzy.py
@@ -0,0 +1,44 @@
+import pygame
+
+pygame.init()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Wizzy')
+
+
+class Soldier(pygame.sprite.Sprite):
+	def __init__(self, x, y, scale):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/hero/Idle/01.png')
+		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+	def draw(self):
+		screen.blit(self.image, self.rect)
+
+
+
+player = Soldier(200, 200, 2)
+
+
+
+run = True
+while run:
+
+
+	player.draw()
+
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+
+
+	pygame.display.update()
+
+pygame.quit()
